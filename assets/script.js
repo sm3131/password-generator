7aa318b3da8f407bafff1ce514a5c1441a1f900a
sm3131 <sm2683@nau.edu>
2021-11-11 20:41:35 -0700

    changed relative link paths for css and js files in html; and added js pseudocode

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,16 @@
 // Assignment code here
-
+// window.prompt for password length:
+  // store length 
+// separate confirm prompts for lowercase, uppercase, numeric, and/or special characters
+  // store options
+// create global word banks (var wor)
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  userPrompt();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
