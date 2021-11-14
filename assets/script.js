e7cff6cdeb41bfd2d035c672d606c850bc3cdb74
sm3131 <sm2683@nau.edu>
2021-11-14 13:05:59 -0700

    fixed bugs and infinite loops; added function calls

--- assets/script.js
+++ assets/script.js
@@ -13,7 +13,7 @@ clearBtn.addEventListener("click", clearPassword);
 
 // Write password to the #password input
 function writePassword() {
-  characterLength();  
+  characterLength();
   characterTypes();
   pword = "";
   pword = generatePassword();
@@ -25,53 +25,54 @@ function clearPassword() {
 }
 
 function characterLength() {
-  passwordLength = window.prompt ("How many characters do you want your password to contain?");
+  passwordLength = window.prompt("How many characters do you want your password to contain?");
   console.log(passwordLength)
 
   if (passwordLength < 8 || passwordLength > 128) {
-    window.alert ("Please enter a character length between 8 and 128.");
+    window.alert("Please enter a character length between 8 and 128.");
+    characterLength();
   }
 }
 
-  function characterTypes() {
+function characterTypes() {
 
   var charTypePrompt = window.alert("Please select at least one character type to include in your password.");
-  
-  lowerCase = window.confirm ("Do you want your password to contain lower case characters?");
+
+  lowerCase = window.confirm("Do you want your password to contain lower case characters?");
   console.log("lowerCase: " + lowerCase);
 
-  upperCase = window.confirm ("Do you want your password to contain upper case characters?");
+  upperCase = window.confirm("Do you want your password to contain upper case characters?");
   console.log("upperCase: " + upperCase);
 
-  numeric = window.confirm ("Do you want your password to contain numbers?");
+  numeric = window.confirm("Do you want your password to contain numbers?");
   console.log("numeric: " + numeric);
 
-  specialCharacters = window.confirm ("Do you want your password to contain special characters?");
+  specialCharacters = window.confirm("Do you want your password to contain special characters?");
   console.log("special: " + specialCharacters);
-  
+
   if ((lowerCase || upperCase || numeric || specialCharacters) === false) {
-  return characterTypes();
+    characterTypes();
   }
-  }
+}
 
 function generatePassword() {
   for (i = 1; i <= passwordLength; i++) {
     notValid = 1
     while (notValid == 1) {
-    getRandomNumber(1,4);
-    switch (characterValue) {
-      case 1:
-        if (lowerCase) {
-          lowerCase = generateLowerCase();
-          pword = pword + lowerCase
-          notValid = 0;
-          console.log(pword);
-          break;
-        }
-        else {
-          notValid = 1;
-          break;
-        }
+      getRandomNumber(1, 4);
+      switch (characterValue) {
+        case 1:
+          if (lowerCase) {
+            lowerCase = generateLowerCase();
+            pword = pword + lowerCase
+            notValid = 0;
+            console.log(pword);
+            break;
+          }
+          else {
+            notValid = 1;
+            break;
+          }
         case 2:
           if (upperCase) {
             upperCase = generateUpperCase();
@@ -82,12 +83,12 @@ function generatePassword() {
           }
           else {
             notValid = 1;
-            break; 
+            break;
           }
         case 3:
           if (numeric) {
-            numeric = generateNumeric(0,9);
-            pword = pword + numeric;
+            number = generateNumeric(0, 9);
+            pword = pword + number;
             notValid = 0;
             console.log(pword);
             break;
@@ -96,40 +97,33 @@ function generatePassword() {
             notValid = 1;
             break;
           }
-          case 4:
-            if (specialCharacters) {
-              specialCharacters = generateSpecialCharacter();
-              pword = pword + specialCharacters;
-              notValid = 0;
-              console.log(pword);
-              break;
-            }
-            else {
-              notValid = 1;
-              break;
-            }
-        }
+        case 4:
+          if (specialCharacters) {
+            specialCharacters = generateSpecialCharacter();
+            pword = pword + specialCharacters;
+            notValid = 0;
+            console.log(pword);
+            break;
+          }
+          else {
+            notValid = 1;
+            break;
+          }
       }
     }
-    return pword
-}
-       
-          
-
-
-      
-
-  var getRandomNumber = function(min, max) {
-    characterValue = Math.floor(Math.random() * (max - min + 1) + min);
-    console.log(characterValue);
-
-
   }
+  return pword
+}
+
+var getRandomNumber = function (min, max) {
+  characterValue = Math.floor(Math.random() * (max - min + 1) + min);
+  console.log(characterValue);
+}
 
 function generateLowerCase() {
   const lowerCaseAlphabet = "abcdefghijklmnopqrstuvwxyz"
-   return lowerCaseAlphabet[Math.floor(Math.random() * lowerCaseAlphabet.length)];
-  }
+  return lowerCaseAlphabet[Math.floor(Math.random() * lowerCaseAlphabet.length)];
+}
 
 function generateUpperCase() {
   const upperCaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
